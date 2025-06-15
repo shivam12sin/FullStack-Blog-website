b9aede2e180aa267d9bcead3b1029447a902cb2c
Shivam Singh <shivam.23scse1010271@galgotiasuniversity.ac.in>
2025-06-15 23:06:07 +0530

    Add session management and authentication middleware; create dashboard view

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,7 +3,27 @@ const router  = express.Router();
 const Post = require('../models/post');
 const User = require('../models/user');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = 
+process.env.JWT_SECRET;
+
+const authMiddleware = (req,res,next)=>{
+  const token = req.cookies.token;
+  if(!token){
+    return res.status(401).json({message:'Unauthorized'});
+  }
+  try{
+    const decoded = jwt.verify(token,jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  }catch(error){
+     res.status(401).json({message:'Unauthorized'});
+
+  }
+}
 
 router.get('/admin',async(req,res)=>{
   try {
@@ -23,10 +43,24 @@ router.get('/admin',async(req,res)=>{
 router.post('/admin',async(req,res)=>{
   try {
     const {username,password} = req.body;
-    console.log(req.body);
+    const user = await User.findOne({username});
+
+    if(!user){
+      return res.status(401).json({message:'Invalid credentials'});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+
+    if(!isPasswordValid){
+      return res.status(401).json({message:'Invalid credentials'});
+    }
+
+
+    const token = jwt.sign({userId:user._id},jwtSecret);
+    res.cookie('token',token,{httpOnly:true});
+
+    res.redirect('/dashboard');
   
-    res.redirect('/admin');
-  // res.render('admin/index',{locals,layout:adminLayout});
     
   } catch (error) {
     console.log(error);
@@ -35,12 +69,36 @@ router.post('/admin',async(req,res)=>{
 });
 
 
+router.get('/dashboard',authMiddleware,async(req,res)=>{
+  res.render('admin/dashboard');
+})
 
 
+router.post('/register',async(req,res)=>{
+  try {
+    const {username,password} = req.body;
+    const hashedPassword = await bcrypt.hash(password,10);
 
+    try {
+      const user = await User.create({username,password:hashedPassword});
+      res.status(201).json({message:'User Created',user});
+      
+    } catch (error) {
+      if(error.code===11000){
+        res.status(409).json({message:'User already in user'})
+      }
+      res.status(500).json({message:'Internal server error'});
+      console.log(error);
+    
+      
+    }
+    
+    
+  } catch (error) {
+    console.log(error);
+  }
 
-
-
+});
 
 
 
